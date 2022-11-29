97962b00c10f9cee3fa2caa91b41aea600ff930e
Thejaswini02 <115609807+Thejaswini02@users.noreply.github.com>
2022-11-29 18:54:15 +0530

    Add files via upload

---
+++ indentity matrix.py
@@ -0,0 +1,5 @@
+idt_mtx=[[1,0,0],
+         [0,1,0],
+         [0,0,1]]
+for r in idt_mtx:
+    print(r)
---
+++ replacing letter with next.py
@@ -0,0 +1,8 @@
+string=str(input(' enter a string'))
+replacestring=''
+for letter in string:
+    code=ord(letter)
+    next_letter=chr(code+1)
+    replacestring=replacestring+ next_letter
+
+print(replacestring)
