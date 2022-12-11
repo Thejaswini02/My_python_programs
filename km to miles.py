612a73b509842ba798aa74fdb379bb38569b488a
Thejaswini02 <115609807+Thejaswini02@users.noreply.github.com>
2022-12-11 22:38:00 +0530

    Add files via upload

--- km to miles.py
+++ km to miles.py
@@ -1,3 +1,3 @@
-a=float(input("Enter the distance in kilometers: "))
-b=a*0.62137
-print("The converted distance in miles:" +str(b))
+kilometers =float(input("Enter the distance in kilometers: "))
+miles=kilometers*0.62137
+print("The converted distance in miles:" +str(miles))
---
+++ middle.py
@@ -0,0 +1,7 @@
+
+def middle(t):
+    t.pop(-1)
+    t.pop(0)
+    return t
+t=[3,4,5,6]
+print(middle(t))
